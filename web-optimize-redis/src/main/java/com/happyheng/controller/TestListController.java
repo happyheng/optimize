304b597ecf96757a8357f4ef774e59644fb6f8bd
happyheng <happyheng9522@163.com>
2017-05-21 12:41:38 +0800

    增加缓存大量列表数据的方法

--- web-optimize-redis/src/main/java/com/happyheng/controller/TestListController.java
+++ web-optimize-redis/src/main/java/com/happyheng/controller/TestListController.java
@@ -1,11 +1,14 @@
 package com.happyheng.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.happyheng.dao.ArticleDao;
 import com.happyheng.model.Article;
 import com.happyheng.model.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 import java.util.List;
 
@@ -17,12 +20,44 @@ import java.util.List;
 @RequestMapping("/testList")
 public class TestListController {
 
+
+    private static final String KEY_CACHE_THOUSAND_ARTICLE_LIST = "article_thousand_list";
+
     @Autowired
     private ArticleDao articleDao;
 
     @RequestMapping("/getArticleList")
-    public List<Article> getArticleList(Page page){
+    public List<Article> getArticleList(Page page) throws Exception{
 
-        return null;
+
+        // 判断页数以及数量是否超标
+        int pageIndex = page.getPageIndex();
+        int pageSize = page.getPageSize();
+        if (pageIndex <= 0 || pageSize <= 0){
+            return null;
+        }
+        if (pageIndex * pageSize > ArticleDao.PAGE_MAX_NUM){
+            return null;
+        }
+
+        // 从redis中获取1000条数据
+        Jedis jedis = new Jedis("localhost");
+        String articleThousandListStr = jedis.get(KEY_CACHE_THOUSAND_ARTICLE_LIST);
+        List<Article> articleList;
+        if (StringUtils.isEmpty(articleThousandListStr)){
+
+            // 如果没有,从数据库中直接取出1000条, 并将其缓存到redis中
+            articleList = articleDao.getThousandArticleListFromdb();
+            jedis.set(KEY_CACHE_THOUSAND_ARTICLE_LIST, JSON.toJSONString(articleList));
+            jedis.expire(KEY_CACHE_THOUSAND_ARTICLE_LIST, 60);
+        } else {
+
+            // 将String反序列化为Article列表
+            articleList = JSON.parseArray(articleThousandListStr, Article.class);
+        }
+
+        // 从列表中选出指定的列表数据
+        List<Article> result = articleList.subList((pageIndex-1) * pageSize, pageIndex * pageSize);
+        return result;
     }
 }
--- web-optimize-redis/src/main/java/com/happyheng/dao/ArticleDao.java
+++ web-optimize-redis/src/main/java/com/happyheng/dao/ArticleDao.java
@@ -15,6 +15,45 @@ import java.util.List;
 @Service
 public class ArticleDao {
 
+    public static final int PAGE_MAX_NUM = 1000;
+
+    public List<Article> getThousandArticleListFromdb() throws Exception{
+
+        String sql = "SELECT * FROM article ORDER BY create_time DESC LIMIT 0, " + PAGE_MAX_NUM;
+
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection mConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/optimize_db", "root", "mytestcon");
+        Statement statement = mConnection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        try {
+            List<Article> list = new ArrayList<>();
+            //注意指针刚开始是-1位置，这行next()方法，会先判断下一个位置有没有，如果有，指向下一个位置。
+            while (resultSet.next()) {
+                Article article = new Article();
+                article.setId(resultSet.getLong("id"));
+                article.setTitle(resultSet.getString("title"));
+                article.setContent(resultSet.getString("content"));
+                article.setCreate_time(resultSet.getString("create_time"));
+                list.add(article);
+            }
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                resultSet.close();
+                statement.close();
+                mConnection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return null;
+    }
+
 
     public List<Article> getFirstPageArticleListFromdb() throws Exception{
 
